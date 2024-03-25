c0b0025bb7b7dc8b3553cb8d30c5e7dcf93059ac
Alex <103727459+spiry32@users.noreply.github.com>
2024-03-25 15:28:09 +0200

    Update app.go
    ver 1.1

--- app.go
+++ app.go
@@ -29,6 +29,8 @@ var categories = map[string][]string{
 	"Media": {
 		"VLC Media Player",
 		"Spotify",
+		"Audacity",
+		"iTunes",
 	},
 	"File Sharing": {
 		"qBittorrent",
@@ -38,17 +40,30 @@ var categories = map[string][]string{
 		"7-Zip",
 		"PeaZip",
 	},
+	"Developer Tools": {
+		"WinSCP",
+		"Notepad++",
+		"PuTTY",
+		"Eclipse",
+		"FileZilla",
+	},
+	"Imaging": {
+		"Krita",
+		"Blender",
+		"GIMP",
+		"Inkscape",
+	},
 }
 
 var applications = map[string]string{
 	// Browsers
-	"Google Chrome":  "https://dl.google.com/chrome/install/8795.700/chrome_installer.exe",
-	"Brave":          "https://laptop-updates.brave.com/latest/winx64",
-	"Vivaldi":        "https://downloads.vivaldi.com/stable/Vivaldi.3.8.2259.37.exe",
-	"Microsoft Edge": "https://go.microsoft.com/fwlink/?linkid=2136346",
-	"Opera":          "https://www.opera.com/download/get/?id=47899&location=360&nothanks=yes&sub=marine",
-	"Opera GX":       "https://www.opera.com/download/get/?id=47899&location=360&nothanks=yes&sub=marine",
-	"Firefox":        "https://www.mozilla.org/firefox/download/thanks/",
+	"Google Chrome [X]":  "https://dl.google.com/chrome/install/8795.700/chrome_installer.exe",
+	"Brave":              "https://laptop-updates.brave.com/latest/winx64",
+	"Vivaldi":            "https://downloads.vivaldi.com/stable/Vivaldi.3.8.2259.37.exe",
+	"Microsoft Edge [X]": "https://go.microsoft.com/fwlink/?linkid=2136346",
+	"Opera":              "https://www.opera.com/download/get/?id=47899&location=360&nothanks=yes&sub=marine",
+	"Opera GX":           "https://www.opera.com/download/get/?id=47899&location=360&nothanks=yes&sub=marine",
+	"Firefox":            "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US&_gl=1*12wy6k4*_ga*NzE1Njk2OTQ0LjE3MDk1NDU0NTQ.*_ga_MQ7767QQQW*MTcxMTM3MDk2MC4zLjEuMTcxMTM3MDk5Mi4wLjAuMA..",
 	// Messaging
 	"Discord": "https://discord.com/api/download?platform=win",
 	"Zoom":    "https://zoom.us/client/latest/ZoomInstaller.exe",
@@ -56,12 +71,25 @@ var applications = map[string]string{
 	// Media
 	"VLC Media Player": "https://get.videolan.org/vlc/3.0.12/win64/vlc-3.0.12-win64.exe",
 	"Spotify":          "https://download.scdn.co/SpotifyFullSetup.exe",
+	"Audacity":         "https://github.com/audacity/audacity/releases/download/Audacity-3.4.2/audacity-win-3.4.2-64bit.exe",
+	"iTunes":           "https://www.apple.com/itunes/download/win64",
 	// File Sharing
 	"qBittorrent": "https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent_4.3.8_setup.exe/download",
 	// Compression
 	"winrar": "https://www.rarlab.com/rar/wrar601.exe",
 	"7-Zip":  "https://www.7-zip.org/a/7z2106-x64.exe",
 	"PeaZip": "https://osdn.net/frs/redir.php?m=netix&f=peazip%2F7.10.0%2Fpeazip_portable-7.10.0.WINDOWS.exe",
+	// Dev  Tools
+	"WinSCP":    "https://winscp.net/download/files/202403251303916f4d129e8c7043e8537e47cb5d5f5b/WinSCP-6.3.2-Setup.exe",
+	"Notepad++": "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6.4/npp.8.6.4.Installer.x64.exe",
+	"PuTTY":     "https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe",
+	"FileZilla": "https://download.filezilla-project.org/client/FileZilla_3.66.5_win64_sponsored2-setup.exe",
+	"Eclipse":   "https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2024-03/R/eclipse-inst-jre-win64.exe",
+	// Imaging
+	"Krita":       "https://download.kde.org/stable/krita/5.2.2/krita-x64-5.2.2-setup.exe",
+	"Blender [X]": "https://www.blender.org/download/release/Blender4.0/blender-4.0.2-windows-x64.msi/",
+	"GIMP":        "https://download.gimp.org/gimp/v2.10/windows/gimp-2.10.36-setup-1.exe",
+	"Inkscape":    "https://inkscape.org/gallery/item/44617/inkscape-1.3.2_2023-11-25_091e20e-x64.exe",
 }
 
 func downloadFile(url, filename string) error {
